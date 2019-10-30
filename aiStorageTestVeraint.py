7ac065cf4e821cc4a6f019946602f87dcb85eab3
George J Shea <52836004+GeorgeJShea@users.noreply.github.com>
2019-10-30 11:09:52 -0500

    Create aiStorageTestVeraint.py

---
+++ aiStorageTestVeraint.py
@@ -0,0 +1,61 @@
+import numpy as np
+import os
+import cv2
+import random
+import pickle
+
+
+
+def create_training_data():
+    DATADIR = "C:\FindImage"
+    CATEGORIES = ["compost", "rcycle", "trash"]
+
+    for category in CATEGORIES:
+        path = os.path.join(DATADIR, category)
+        for img in os.listdir(path):
+            img_array = cv2.imread(os.path.join(path, img), cv2.IMREAD_GRAYSCALE)
+            break
+        break
+
+    training_data = []
+
+    for category in CATEGORIES:
+
+        path = os.path.join(DATADIR,category)
+        class_num = CATEGORIES.index(category)
+
+        for img in os.listdir(path):
+            try:
+                img_array = cv2.imread(os.path.join(path,img) ,cv2.IMREAD_GRAYSCALE)
+                IMG_SIZE = 50
+                new_array = cv2.resize(img_array, (IMG_SIZE, IMG_SIZE))
+                new_array = new_array/255
+                training_data.append([new_array, class_num])
+            except Exception as e:
+                pass
+
+
+    random.shuffle(training_data)
+
+    for features, label in training_data:
+        X = []
+        y = []
+
+        X.append(features)
+        y.append(label)
+
+        #-------------------------------------------------
+        X = np.array(X).reshape(-1, IMG_SIZE, IMG_SIZE)
+
+        pickle_out = open("testX.pickle", "wb")
+        pickle.dump(X, pickle_out)
+        pickle_out.close()
+
+        pickle_out = open("testy.pickle", "wb")
+        pickle.dump(y, pickle_out)
+        pickle_out.close()
+
+
+create_training_data()
+
+print("Ai Storage veriant been stored || Rev1 ||")
